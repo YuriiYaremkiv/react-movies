a553fbe086982578a294598cbcddd5ba4b86e3de
yyaremkiv <y.yaremkiv@gmail.com>
2022-12-23 15:31:00 +0200

    add_search

--- project/src/App.js
+++ project/src/App.js
@@ -1,12 +1,14 @@
 import { Header } from "./layout/Header";
 import { Main } from "./layout/Main";
 import { Footer } from "./layout/Footer";
+import { Movies } from "./components/Movies";
 
 export const App = () => {
   return (
     <>
       <Header />
       <Main />
+      <Movies />
       <Footer />
     </>
   );
--- project/src/layout/Main.jsx
+++ project/src/layout/Main.jsx
@@ -1,3 +1,34 @@
-export const Main = () => {
-  return <main className="container content">Hello from Movies App</main>;
-};
+import React from "react";
+import { Movies } from "../components/Movies";
+import { Preloader } from "../components/Preloader";
+import { Search } from "../components/Search";
+
+export class Main extends React.Component {
+  state = {
+    movies: [],
+    search: "",
+  };
+
+  searchMovies = (str) => {
+    fetch(`http://www.omdbapi.com/?apikey=34e3dba2&s=${str}`)
+      .then((response) => response.json())
+      .then((data) => this.setState({ movies: data.Search }));
+  };
+
+  componentDidMount() {
+    fetch("http://www.omdbapi.com/?apikey=34e3dba2&s=matrix")
+      .then((response) => response.json())
+      .then((data) => this.setState({ movies: data.Search }));
+  }
+
+  render() {
+    const { movies } = this.state;
+
+    return (
+      <main className="container content">
+        <Search searchMovies={this.searchMovies} />
+        {movies.length ? <Movies movies={movies} /> : <Preloader />}
+      </main>
+    );
+  }
+}
